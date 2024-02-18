cbf66cf82763d795a7ce2d71b19d8ab59503edc2
Haeun Noh <00blowup@gmail.com>
2024-02-18 15:54:11 +0900

    fix: 정답 정수를 랜덤 생성할 때에 각 자리 숫자가 서로 중복되지 않도록 설정

--- src/main/java/baseball/util/RandomUtil.java
+++ src/main/java/baseball/util/RandomUtil.java
@@ -3,12 +3,19 @@ package baseball.util;
 import baseball.vo.Answer;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RandomUtil {
 
     public Answer generateAnswer() {
-        Answer answer = new Answer(Randoms.pickNumberInRange(1,9),
-                Randoms.pickNumberInRange(1,9),
-                Randoms.pickNumberInRange(1,9));
+        List<Integer> oneToNine = new ArrayList<>();
+        for(int i=1; i<10; i++) oneToNine.add(i);
+
+        Answer answer = new Answer(oneToNine.remove(Randoms.pickNumberInRange(0,8)),
+                oneToNine.remove(Randoms.pickNumberInRange(0,7)),
+                oneToNine.remove(Randoms.pickNumberInRange(0,6)));
 
         return answer;
     }
