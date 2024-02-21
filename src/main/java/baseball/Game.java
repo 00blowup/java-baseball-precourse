51b7b3c6e403636d360a34f66c634bb646cbe752
Haeun Noh <00blowup@gmail.com>
2024-02-21 18:30:00 +0900

    feat: 사용자가 입력한 수를 정답 정수와 비교하여 스트라이크와 볼의 개수를 구하는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.domain.Answer;
+import baseball.domain.Hint;
 import baseball.domain.Input;
 import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -29,4 +30,23 @@ public class Game {
     public Input getGuessingNumberInput() {
         return InputView.readGuessingNumber();
     }
+
+    public Hint calculateHint(Answer answer, Input guessingInput) {
+        List<Integer> answerDigits = answer.getDigits();
+        List<Integer> inputDigits = guessingInput.getDigits();
+
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for (int i = 0; i < Answer.LENGTH_OF_ANSWER; i++) {
+            if (answerDigits.get(i).equals(inputDigits.get(i))) {
+                strikeCount++;
+                continue;
+            }
+            if (inputDigits.contains(answerDigits.get(i))) ballCount++;
+        }
+
+        return new Hint(ballCount, strikeCount);
+    }
+
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,8 +1,13 @@
 package baseball;
 
 import baseball.domain.Answer;
+import baseball.domain.Hint;
 import baseball.domain.Input;
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.util.*;
@@ -10,6 +15,16 @@ import java.util.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
+
+    public static List<String> generateRandomInputString() {
+
+        List<String> inputStrings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            inputStrings.add(Integer.toString(Randoms.pickNumberInRange(100, 999)));
+        }
+
+        return inputStrings;
+    }
 
     @Test
     void 생성된_정답이_세_자리_정수이다() {
@@ -34,6 +49,55 @@ class GameTest {
         List<Integer> inputDigits = game.getGuessingNumberInput().getDigits();
         assertThat(inputDigits).hasSize(Answer.LENGTH_OF_ANSWER);
         assertThat(inputDigits.stream().allMatch(n -> n >= 1 && n <= 9)).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateRandomInputString")
+    void 힌트_계산_테스트(String inputString) {
+        Game game = new Game();
+
+        Answer answer = game.createAnswer();
+        List<Integer> answerDigits = answer.getDigits();
+
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        Input guessingInput = game.getGuessingNumberInput();
+        List<Integer> inputDigits = guessingInput.getDigits();
+
+        Hint calculatedHint = game.calculateHint(answer, guessingInput);
+
+        int correctBallCount = 0;
+        int correctStrikeCount = 0;
+
+        for (int i = 0; i < Answer.LENGTH_OF_ANSWER; i++) {
+            if (answerDigits.get(i).equals(inputDigits.get(i))) {
+                correctStrikeCount++;
+                continue;
+            }
+            if (inputDigits.contains(answerDigits.get(i))) correctBallCount++;
+        }
+
+        assertThat(calculatedHint.getBallCount()).isEqualTo(correctBallCount);
+        assertThat(calculatedHint.getStrikeCount()).isEqualTo(correctStrikeCount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"444", "555", "666"})
+    void 낫싱_테스트(String inputString) {
+        Game game = new Game();
+
+        List<Integer> answerDigits = new ArrayList<>();
+        answerDigits.add(1);
+        answerDigits.add(2);
+        answerDigits.add(3);
+        Answer answer = new Answer(answerDigits);
+
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        Input guessingInput = game.getGuessingNumberInput();
+
+        Hint calculatedHint = game.calculateHint(answer, guessingInput);
+
+        assertThat(calculatedHint.getBallCount()).isEqualTo(0);
+        assertThat(calculatedHint.getStrikeCount()).isEqualTo(0);
     }
 
 }
